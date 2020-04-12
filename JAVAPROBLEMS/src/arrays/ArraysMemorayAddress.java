d13843a7ad03cfd0c331d0af8b9e5b6cc52dc72a
v04jais <vjaiswal604@gmail.com>
2020-04-12 16:53:01 +0530

    mapping local to repo

---
+++ JAVAPROBLEMS/src/arrays/ArraysMemorayAddress.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class ArraysMemorayAddress {
+
+	public static void main(String[] args) {
+		
+		
+		
+		/*
+		 * fixed size array register in stack area in c and c++ while always in heap area in java
+		 * to get y element from arr address say x we need to add ysize to address x..i.e x+y
+		 * Dynamic array in java is arraylist while in c++ is vector and list in python
+		 * Dynamic array doubles the size when its capacity is reached
+		 */
+		int arr[]= new int[0];
+		System.out.println(arr);
+
+	}
+
+}
